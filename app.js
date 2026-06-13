0a256fd36c143346f25a6d41e3aad12c552d587b
diegoFonseca <difc.technology@gmail.com>
2026-06-12 23:25:32 -0500

    modificación

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
-let botonesAgregar = document.querySelectorAll('.btn-agregar');
+let botonesAgregar = document.querySelectorAll(".btn-agregar");
+let cantidadItems = 0;
+let carritoCompra = [];
 
 botonesAgregar.forEach((boton, index) => {
-  boton.addEventListener('click',function(){
-  console.log(boton.dataset.nombre);
-  console.log(boton.dataset.precio);  
+  boton.addEventListener("click", function () {
+    let nombre = boton.dataset.nombre;
+    let precio = boton.dataset.precio;
+    agregarAlCarrito(nombre, precio);
   });
 });
+
+function agregarAlCarrito(nombre, precio) {
+  let producto = carritoCompra.find((producto) => producto.nombre == nombre);
+  if (producto == undefined) {
+    producto = new Producto(nombre, precio, 1);
+    let li = document.createElement("li");
+    li.id=nombre;
+    li.innerHTML = `
+    ${nombre} ${precio} ${producto.cantidad}<input type="button" value="Eliminar" class="btn btn-danger"/>
+    `;
+    let carrito = document.getElementById("lista-carrito");
+    carrito.appendChild(li);
+    carritoCompra.push(producto);
+  }else{
+    producto.cantidad++;
+    let li = document.getElementById(nombre);
+    li.innerHTML = `
+    ${nombre} ${precio} ${producto.cantidad}<input type="button" value="Eliminar" class="btn btn-danger"/>
+    `;
+  }
+  cantidadItems++;
+      updateBadge();
+}
+
+function updateBadge() {
+  badge = document.getElementById("badge");
+  console.log(badge);
+  badge.textContent = cantidadItems;
+}
+
+class Producto {
+  constructor(nombre, precio, cantidad) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.cantidad = cantidad;
+  }
+}
